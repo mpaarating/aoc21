c9489117d45d7fad7a37018550510aa9f040f48b
Alex Lewis <alexmichael.lewis@gmail.com>
2021-12-05 15:51:16 -0500

    02-part2-solution

--- 02/02.js
+++ 02/02.js
@@ -1,41 +1,77 @@
 import separateCommandAndAmount from './utils/separateCommandAndAmount.js';
 
-/**
- * Calculates the current position from a given starting point and a given movement instruction
- * @param {number[]} position - Current position in horizontal and depth [horizontal, depth]
- * @param {object} instruction - Movement command
- * @param {string} command.command - Direction of movement
- * @param {number} command.amount - Number of steps
- * @returns {number[]} - New position in horizontal and depth [horizontal, depth]
- */
-export function calculatePosition(position, instruction) {
-  let [horizontal, depth] = position;
-  switch (instruction.command) {
-    case 'forward':
-      horizontal += instruction.amount;
-      break;
-    case 'up':
-      depth -= instruction.amount;
-      break;
-    case 'down':
-      depth += instruction.amount;
-      break;
-  }
-  return [horizontal, depth];
-}
+const forward = ([horizontal, depth], amount) => [horizontal + amount, depth];
+const up = ([horizontal, depth], amount) => [horizontal, depth - amount];
+const down = ([horizontal, depth], amount) => [horizontal, depth + amount];
+
+const forwardWithAim = ([horizontal, depth], amount, aim) => [
+  horizontal + amount,
+  aim === 0 ? depth : amount * aim,
+  aim,
+];
+const upWithAim = ([horizontal, depth], amount, aim) => [
+  horizontal,
+  depth,
+  aim - amount,
+];
+const downWithAim = ([horizontal, depth], amount, aim) => [
+  horizontal,
+  depth,
+  aim + amount,
+];
+
+export const withAimCaseInstructions = {
+  forward: forwardWithAim,
+  up: upWithAim,
+  down: downWithAim,
+};
+
+export const withoutAimCaseInstructions = {
+  forward,
+  up,
+  down,
+};
+
+export const executeMovement =
+  (caseInstructions) => (position, instruction, aim) =>
+    caseInstructions[instruction.command](position, instruction.amount, aim);
 
 /**
  * Returns the product of the horizontal position and depth from the array of commands.
  * @param {string[]} commands - The array of commands.
- * @returns {number} product - The product of the horizontal position and depth.
+ * @returns {number} The product of the horizontal position and depth.
  */
 export function part1(commands) {
+  const calculatePosition = executeMovement(withoutAimCaseInstructions);
   const parsedCommands = separateCommandAndAmount(commands);
   let position = [0, 0];
-  let result = 0;
   for (let command of parsedCommands) {
     position = calculatePosition(position, command);
-    result = Math.max(result, position[0] * position[1]);
   }
-  return result;
+  const [horizontal, depth] = position;
+  return horizontal * depth;
+}
+
+/**
+ * Returns the product of the horizontal position and depth from the array of commands including aim
+ * @param {string[]} commands - The array of commands.
+ * return {number} product - The product of the horizontal position and depth.
+ */
+export function part2(commands) {
+  const calculatePosition = executeMovement(withAimCaseInstructions);
+  const parsedCommands = separateCommandAndAmount(commands);
+  let position = [0, 0];
+  let horizontal = 0;
+  let depth = 0;
+  let aim = 0;
+  let result = 0;
+  for (let command of parsedCommands) {
+    [horizontal, depth, aim] = calculatePosition(
+      [horizontal, depth],
+      command,
+      aim
+    );
+  }
+
+  return horizontal * depth;
 }
--- 02/02.test.js
+++ 02/02.test.js
@@ -1,16 +1,88 @@
-import { calculatePosition, part1 } from './02.js';
+import {
+  part1,
+  part2,
+  executeMovement,
+  withAimCaseInstructions,
+  withoutAimCaseInstructions,
+} from './02.js';
 import separateCommandAndAmount from './utils/separateCommandAndAmount.js';
 import input from './input.json';
 
 describe('Day 2', () => {
   describe('calculatePosition', () => {
-    it('returns the correct positions', () => {
-      const rawTestInput = ['forward 1'];
-      const testCommandInput = separateCommandAndAmount(rawTestInput)[0];
-      const startingPosition = [0, 0];
-      const expected = [1, 0];
-      const actual = calculatePosition(startingPosition, testCommandInput);
-      expect(actual).toEqual(expected);
+    describe('without aim', () => {
+      it('calculates the correct position for forward movement', () => {
+        const calculatePosition = executeMovement(withoutAimCaseInstructions);
+        const rawTestInput = ['forward 10'];
+        const testCommandInput = separateCommandAndAmount(rawTestInput)[0];
+        const startingPosition = [0, 0];
+        const expected = [10, 0];
+        const actual = calculatePosition(startingPosition, testCommandInput);
+        expect(actual).toEqual(expected);
+      });
+      it('calculates the correct position for upward movement', () => {
+        const calculatePosition = executeMovement(withoutAimCaseInstructions);
+        const rawTestInput = ['up 10'];
+        const testCommandInput = separateCommandAndAmount(rawTestInput)[0];
+        const startingPosition = [0, 0];
+        const expected = [0, -10];
+        const actual = calculatePosition(startingPosition, testCommandInput);
+        expect(actual).toEqual(expected);
+      });
+      it('calculates the correct position for downward movement', () => {
+        const calculatePosition = executeMovement(withoutAimCaseInstructions);
+        const rawTestInput = ['down 10'];
+        const testCommandInput = separateCommandAndAmount(rawTestInput)[0];
+        const startingPosition = [0, 0];
+        const expected = [0, 10];
+        const actual = calculatePosition(startingPosition, testCommandInput);
+        expect(actual).toEqual(expected);
+      });
+    });
+
+    describe('with aim', () => {
+      it('calculates the correct position and aim for forward movement', () => {
+        const calculatePosition = executeMovement(withAimCaseInstructions);
+        const rawTestInput = ['forward 10'];
+        const testCommandInput = separateCommandAndAmount(rawTestInput)[0];
+        const startingPosition = [3, 4];
+        const aim = 3;
+        const expected = [13, 30, 3];
+        const actual = calculatePosition(
+          startingPosition,
+          testCommandInput,
+          aim
+        );
+        expect(actual).toEqual(expected);
+      });
+      it('calculates the correct position and aim for upward movement', () => {
+        const calculatePosition = executeMovement(withAimCaseInstructions);
+        const rawTestInput = ['up 10'];
+        const testCommandInput = separateCommandAndAmount(rawTestInput)[0];
+        const startingPosition = [3, 4];
+        const aim = 3;
+        const expected = [3, 4, -7];
+        const actual = calculatePosition(
+          startingPosition,
+          testCommandInput,
+          aim
+        );
+        expect(actual).toEqual(expected);
+      });
+      it('calculates the correct position and aim for downward movement', () => {
+        const calculatePosition = executeMovement(withAimCaseInstructions);
+        const rawTestInput = ['down 10'];
+        const testCommandInput = separateCommandAndAmount(rawTestInput)[0];
+        const startingPosition = [3, 4];
+        const aim = 3;
+        const expected = [3, 4, 13];
+        const actual = calculatePosition(
+          startingPosition,
+          testCommandInput,
+          aim
+        );
+        expect(actual).toEqual(expected);
+      });
     });
   });
 
@@ -36,6 +108,25 @@ describe('Day 2', () => {
       it('returns the product of the current position after a series of movement commands', () => {
         const expected = 1604850;
         const actual = part1(input);
+        expect(actual).toEqual(expected);
+      });
+    });
+  });
+
+  describe('part2', () => {
+    describe('TDD base case', () => {
+      it('returns the product of the current position after a series of movement commands', () => {
+        const testInput = ['forward 1', 'down 2', 'forward 1'];
+        const expected = 4;
+        const actual = part2(testInput);
+        expect(actual).toEqual(expected);
+      });
+    });
+
+    describe('Real input', () => {
+      it('returns the product of the current position after a series of movement commands', () => {
+        const expected = 4814550;
+        const actual = part2(input);
         expect(actual).toEqual(expected);
       });
     });
